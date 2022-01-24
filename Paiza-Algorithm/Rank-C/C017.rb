28b7a6685e0d21776770755f2f4b999e8018bed4
kota <kota200016@icloud.com>
2022-01-24 10:00:14 +0900

    PaizaC017解答

---
+++ Paiza-Algorithm/Rank-C/C017.rb
@@ -0,0 +1,20 @@
+input_line = readlines
+
+parent_card_nums = input_line[0].split(' ')
+children_card_array = input_line[2, input_line[1].to_i + 2]
+result = []
+
+for num in 1..input_line[1].to_i
+    children_card_nums = children_card_array[num - 1].split(' ')
+
+    if parent_card_nums[0].to_i > children_card_nums[0].to_i
+        result.push('High')
+    elsif parent_card_nums[0] == children_card_nums[0] &&
+            parent_card_nums[1].to_i < children_card_nums[1].to_i
+        result.push('High')
+    else
+        result.push('Low')
+    end
+end
+
+puts result
