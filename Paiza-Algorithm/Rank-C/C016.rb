668e4a5e6da18f3c7a351d393fd486bd3790073d
kota <kota200016@icloud.com>
2022-01-25 10:20:46 +0900

    PaizaC016解答修正

--- Paiza-Algorithm/Rank-C/C016.rb
+++ Paiza-Algorithm/Rank-C/C016.rb
@@ -1,11 +1,10 @@
-# 自分の得意な言語で
 input_line = gets.split('')
 result = ''
-leet = { 'A': '4', 'E': '3', 'G': '6', 'I': '1', 'O': '0', 'S': '5', 'Z': '2' }
+leet = { A: '4', E: '3', G: '6', I: '1', O: '0', S: '5', Z: '2' }
 
 input_line.each do |str|
-    if leet.keys.find { |key| key == str.intern }
-        result += leet[str.intern].to_s
+    if leet.keys.find { |key| key == str.to_sym }
+        result += leet[str.to_sym] # String.internでも同じ処理
     else
         result += str
     end
