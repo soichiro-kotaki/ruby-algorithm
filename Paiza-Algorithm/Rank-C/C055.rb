1b251d9411bf4a26f060460df5c80e57a1573ea6
kota <kota200016@icloud.com>
2022-01-26 10:40:35 +0900

    PaizaC055解答

---
+++ Paiza-Algorithm/Rank-C/C055.rb
@@ -0,0 +1,12 @@
+input_line = readlines.map { |n| n.chomp }
+result = []
+
+for num in 1..input_line[0].to_i
+    if input_line[num + 1].include?("#{input_line[1]}")
+        result.push(input_line[num + 1])
+    end
+end
+
+result.push('None') if result == []
+
+puts result
