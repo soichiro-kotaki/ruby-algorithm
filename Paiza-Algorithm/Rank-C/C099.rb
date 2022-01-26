7dc058f545bbc2fa18571b35d82ff1526f2e417b
kota <kota200016@icloud.com>
2022-01-26 11:05:06 +0900

    PaizaC099解答リファクタリング

--- Paiza-Algorithm/Rank-C/C099.rb
+++ Paiza-Algorithm/Rank-C/C099.rb
@@ -1,9 +1,8 @@
 input_line = readlines
-sheets_num = input_line[0].split(' ')[0].to_i
-sheets_length = input_line[0].split(' ')[1].to_i
+sheets_num, sheets_length = input_line[0].split(' ').map { |n| n.to_i }
 total_length = sheets_length
 
-for num in 1..sheets_num - 1
+for num in 1...sheets_num
     total_length += (sheets_length - input_line[num].to_i)
 end
 
