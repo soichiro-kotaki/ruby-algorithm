3b0b5ae1ee2cc8f66091d6c795630ad2c4374cb1
kota <kota200016@icloud.com>
2022-02-21 09:56:15 +0900

    C022解答修正

--- Paiza-Algorithm/Rank-C/C022.rb
+++ Paiza-Algorithm/Rank-C/C022.rb
@@ -32,7 +32,7 @@ class HighgPrice
     def get_highg_price(share_price_on_all_days)
         array_of_high_price_on_each_days = []
         share_price_on_all_days.each { |share_price_on_each_days| array_of_high_price_on_each_days.push(share_price_on_each_days[2]) }
-        high_price = array_of_high_price_on_each_days.max
+        high_price = array_of_high_price_on_each_days.uniq.sort.max
     end
 end
 
@@ -40,7 +40,9 @@ class LowPrice
     def get_low_price(share_price_on_all_days)
         array_of_low_price_on_each_days = []
         share_price_on_all_days.each { |share_price_on_each_days| array_of_low_price_on_each_days.push(share_price_on_each_days[3]) }
-        low_price = array_of_low_price_on_each_days.min
+        sorted_array_of_low_price_on_each_days = array_of_low_price_on_each_days.sort { |a, b| a.to_i <=> b.to_i }
+        low_price = sorted_array_of_low_price_on_each_days[0]
+    end
 end
 
 input_line = readlines.map { |num| num.chomp }
@@ -48,7 +50,6 @@ input_line = readlines.map { |num| num.chomp }
 num_of_days = GetDays.new(input_line[0].to_i)
 share_price_on_each_day = DivideSharePriceOnEachDay.new(input_line[1..-1])
 share_price_on_each_day_intger = share_price_on_each_day.convert_into_intger
-
 opening_price = OpeningPrice.new.get_opening_price(share_price_on_each_day_intger[0])
 closing_price = ClosingPrice.new.get_closing_price(share_price_on_each_day_intger[num_of_days.num_of_days - 1])
 high_price = HighgPrice.new.get_highg_price(share_price_on_each_day_intger)
