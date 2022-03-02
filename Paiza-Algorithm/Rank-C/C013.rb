a815e38f7be415d9dfb229b1e6f474eca4c6272a
kota <kota200016@icloud.com>
2022-03-02 11:32:53 +0900

    C013解答

--- Paiza-Algorithm/Rank-C/C013.rb
+++ Paiza-Algorithm/Rank-C/C013.rb
@@ -1,8 +1,8 @@
 input_line = readlines.map { |num| num.chomp }
 result = []
-for num in 1..input_line[1].to_i
-    is_included = input_line[num + 1].include?(input_line[0])
-    result.push(input_line[num + 1].to_i) if !is_included
+for num in 2..input_line[1].to_i
+    is_included = input_line[num].include?(input_line[0])
+    result.push(input_line[num].to_i) if !is_included
 end
 
 result == [] && result.push('none')
