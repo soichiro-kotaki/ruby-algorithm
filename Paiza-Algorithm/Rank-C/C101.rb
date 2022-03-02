62f13d00431e121e6c4858fae001fac183c76bbb
kota <kota200016@icloud.com>
2022-03-02 11:33:01 +0900

    C101解答

---
+++ Paiza-Algorithm/Rank-C/C101.rb
@@ -0,0 +1,14 @@
+# ラッキーデイの計算
+class LuckyDay
+    def calc_lucky_days(input)
+        num_of_lucky_day = 0
+
+        for num in 0..365
+            num_of_lucky_day += 1 if num.to_s.include?(input)
+        end
+
+        puts num_of_lucky_day
+    end
+end
+
+LuckyDay.new.calc_lucky_days(gets.chomp)
