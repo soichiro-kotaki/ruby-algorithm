6ed86d0cd920e2a6e89925f99ff56aa799a384c7
kota <kota200016@icloud.com>
2022-03-04 12:00:59 +0900

    C085解答

---
+++ Paiza-Algorithm/Rank-C/C085.rb
@@ -0,0 +1,23 @@
+# 壊れかけのキーボード
+class KeyBoard
+    def calc_alphabet_shown_display(lines)
+        durability_on_each_alphabet_keyboard = lines[0].split(' ').map(&:to_i)
+        entered_alphabet = lines[1].split('')
+        array_of_alphabet = [*'a'..'z']
+        output_on_display = ''
+
+        for num in 0...entered_alphabet.size
+            alphabet_index = array_of_alphabet.find_index(entered_alphabet[num])
+            if durability_on_each_alphabet_keyboard[alphabet_index] > 0
+                output_on_display += entered_alphabet[num]
+                durability_on_each_alphabet_keyboard[alphabet_index] -= 1
+            else
+                next
+            end
+        end
+
+        puts output_on_display
+    end
+end
+
+KeyBoard.new.calc_alphabet_shown_display(readlines.map(&:chomp))
